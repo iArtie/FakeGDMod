54e6e4c1f0e004b445181cb22d137f70822d06d6
BitZ <arturo.chavez92u@std.uni.edu.ni>
2024-06-21 02:26:36 -0600

    Agregar archivos de proyecto.

---
+++ about.md
@@ -0,0 +1,23 @@
+# FakeGDMod
+
+Do you want to be a moderator in geometry dash? <cy>WELL YOU CAN BE IT!</c>
+
+FakeGDMod is made for fun, it doesn't really do anything other than pretend you're a real moderator
+
+This contains the 3 different types of mods:
+
+- **Moderator** ![moderator](frame:modBadge_01_001.png&scale:0.5)
+
+This allows you to send levels and delete levels, in addition to having the moderator badge on your profile
+
+- **Elder Moderator** ![eldermoderator](frame:modBadge_02_001.png&scale:0.5)
+
+This is all of the above and you can additionally delete comments from other users both on the profile and on the levels, even those of the same <cr>RobTop</c>
+
+- **Leaderboard Moderator** ![lbmoderator](frame:modBadge_03_001.png&scale:0.5)
+
+This really doesn't do anything more than have the badge within the game, since all the management of the leaderboards is done externally to the game
+
+## Thanks to
+
+- [Xanii](https://x.com/_Xanii_) for the idea and some tips
---
+++ src/main.cpp
@@ -0,0 +1,144 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+#include <Geode/modify/SupportLayer.hpp>
+#include <Geode/modify/ProfilePage.hpp>
+#include <Geode/modify/RateStarsLayer.hpp>
+#include <Geode/modify/RateDemonLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+class modCheck
+{
+public:
+
+	void DelayMod(CCObject* sender)
+	{
+		UploadActionPopup* modCheck = (UploadActionPopup*)sender;
+
+		
+
+		if (Mod::get()->getSettingValue<int64_t>("modType") == 1)
+		{
+			modCheck->showSuccessMessage("Success! Moderator access granted.");
+		}
+		if (Mod::get()->getSettingValue<int64_t>("modType") == 2)
+		{
+			modCheck->showSuccessMessage("Success! Elder Moderator \n access granted.");
+		}
+	}
+
+	void DelayRate(CCObject* sender)
+	{
+		UploadActionPopup* modCheck = (UploadActionPopup*)sender;
+
+			modCheck->showSuccessMessage("Rating submitted!");
+		
+		
+	}
+};
+class $modify(SupportLayer) {
+	void onRequestAccess(CCObject* sender) {
+
+	
+
+		if (Mod::get()->getSettingValue<int64_t>("modType") == 3)
+		{
+			SupportLayer::onRequestAccess(sender);
+		}
+		else
+		{
+			auto GM = GameManager::sharedState();
+			UploadActionPopup* popup = UploadActionPopup::create(nullptr, "Loading...");
+			popup->show();
+			popup->runAction((CCSequence::create(
+				CCDelayTime::create(1),
+				CCCallFunc::create(this, callfunc_selector(modCheck::DelayMod)),
+				nullptr
+			)));
+
+			GM->m_hasRP = Mod::get()->getSettingValue<int64_t>("modType");
+
+		}
+		
+		
+
+	}
+
+
+	
+	
+};
+
+
+
+class $modify(ProfilePage)
+{
+	void loadPageFromUserInfo(GJUserScore* p0)
+	{
+		int type = Mod::get()->getSettingValue<int64_t>("modType");
+		if (this->m_ownProfile)
+		{
+			
+			if (type == 1)
+			{
+				p0->m_modBadge = 1;
+			}
+			if (type == 2)
+			{
+				p0->m_modBadge = 2;
+			}
+			if (type == 3)
+			{
+				p0->m_modBadge = 3;
+			}
+		}
+		
+		ProfilePage::loadPageFromUserInfo(p0);
+
+		
+	}
+};
+
+class $modify(RateStarsLayer)
+{
+	void onRate(CCObject* sender)
+	{
+		UploadActionPopup* popup = UploadActionPopup::create(nullptr, "Sending rating...");
+		popup->show();
+		popup->runAction((CCSequence::create(
+			CCDelayTime::create(1),
+			CCCallFunc::create(this, callfunc_selector(modCheck::DelayRate)),
+			nullptr
+		)));
+	}
+};
+
+class $modify(RateDemonLayer)
+{
+	void onRate(CCObject * sender)
+	{
+		UploadActionPopup* popup = UploadActionPopup::create(nullptr, "Sending rating...");
+		popup->show();
+		popup->runAction((CCSequence::create(
+			CCDelayTime::create(1),
+			CCCallFunc::create(this, callfunc_selector(modCheck::DelayRate)),
+			nullptr
+		)));
+	}
+};
+
+class $modify(LevelInfoLayer)
+{
+	void levelDeleteFailed(int a1)
+	{
+		auto scene = CCDirector::get()->getRunningScene();
+		LevelInfoLayer::levelDeleteFailed(a1);
+
+		
+
+		auto obj = (FLAlertLayer*)scene->getChildren()->lastObject();
+
+		
+		FLAlertLayer::create("Level Deleted", "The level has been removed from the server", "OK")->show();
+		scene->removeChild(obj);
+	}
+};
